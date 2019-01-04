cbe405dc0887b17899f4b35bab17508da11a7789
andyzhangx <xiazhang@microsoft.com>
2019-01-04 03:24:49 +0000

    fix test failure

--- pkg/csi-common/nodeserver-default_test.go
+++ pkg/csi-common/nodeserver-default_test.go
@@ -22,8 +22,6 @@ import (
 
 	"github.com/container-storage-interface/spec/lib/go/csi/v0"
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func TestNodeGetId(t *testing.T) {
@@ -59,30 +57,4 @@ func TestNodeGetCapabilities(t *testing.T) {
 	req := csi.NodeGetCapabilitiesRequest{}
 	_, err := ns.NodeGetCapabilities(context.Background(), &req)
 	assert.NoError(t, err)
-}
-
-func TestNodePublishVolume(t *testing.T) {
-	d := NewFakeDriver()
-
-	ns := NewDefaultNodeServer(d)
-
-	// Test invalid request
-	req := csi.NodePublishVolumeRequest{}
-	_, err := ns.NodePublishVolume(context.Background(), &req)
-	s, ok := status.FromError(err)
-	assert.True(t, ok)
-	assert.Equal(t, s.Code(), codes.Unimplemented)
-}
-
-func TestNodeUnpublishVolume(t *testing.T) {
-	d := NewFakeDriver()
-
-	ns := NewDefaultNodeServer(d)
-
-	// Test invalid request
-	req := csi.NodeUnpublishVolumeRequest{}
-	_, err := ns.NodeUnpublishVolume(context.Background(), &req)
-	s, ok := status.FromError(err)
-	assert.True(t, ok)
-	assert.Equal(t, s.Code(), codes.Unimplemented)
 }
