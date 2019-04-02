ee76725e74c003700f3baea1b342c2f114b634d2
Ashish Ranjan <ashishranjan738@gmail.com>
2019-04-02 19:13:10 +0530

    refactor(util): remove parse endpoint function
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>
    
    This commit removes parse endpoint function from util package

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -16,15 +16,6 @@ limitations under the License.
 
 package util
 
-import (
-	"fmt"
-	"net/url"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
-)
-
 const (
 	GiB = 1024 * 1024 * 1024
 )
@@ -51,30 +42,15 @@ func GiBToBytes(volumeSizeGiB int64) int64 {
 	return volumeSizeGiB * GiB
 }
 
-func ParseEndpoint(endpoint string) (string, string, error) {
-	u, err := url.Parse(endpoint)
-	if err != nil {
-		return "", "", fmt.Errorf("could not parse endpoint: %v", err)
-	}
-
-	addr := path.Join(u.Host, filepath.FromSlash(u.Path))
-
-	scheme := strings.ToLower(u.Scheme)
-	switch scheme {
-	case "tcp":
-	case "unix":
-		addr = path.Join("/", addr)
-		if err := os.Remove(addr); err != nil && !os.IsNotExist(err) {
-			return "", "", fmt.Errorf("could not remove unix domain socket %q: %v", addr, err)
-		}
-	default:
-		return "", "", fmt.Errorf("unsupported protocol: %s", scheme)
-	}
-
-	return scheme, addr, nil
-}
-
-// TODO: check division by zero and int overflow
+// roundUpSize calculates how many allocation units are needed to accommodate
+// a volume of given size. E.g. when user wants 1500MiB volume, while AWS EBS
+// allocates volumes in gibibyte-sized chunks,
+// RoundUpSize(1500 * 1024*1024, 1024*1024*1024) returns '2'
+// (2 GiB is the smallest allocatable volume that can hold 1500MiB)
 func roundUpSize(volumeSizeBytes int64, allocationUnitBytes int64) int64 {
-	return (volumeSizeBytes + allocationUnitBytes - 1) / allocationUnitBytes
+	roundedUp := volumeSizeBytes / allocationUnitBytes
+	if volumeSizeBytes%allocationUnitBytes > 0 {
+		roundedUp++
+	}
+	return roundedUp
 }
