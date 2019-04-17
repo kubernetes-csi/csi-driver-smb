48ab5e0c7f838f53aa923bce802bb7ef73096283
Ashish Ranjan <ashishranjan738@gmail.com>
2019-04-17 20:19:34 +0530

    enhance(MakeFile): adds `e2e-test` target in MakeFile
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>
    
    This commit adds `e2e-test` target for triggering e2e test

---
+++ hack/run-e2e-test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -uo pipefail
+
+kubectl get daemonsets csi-azurefile -n kube-system; checkAzureFileDriver=$?;
+if [ $checkAzureFileDriver -ne 0 ]; then
+    echo "AzureFile csi driver daemonset not found";
+    echo "Installing AzureFile csi driver";
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/crd-csi-driver-registry.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/crd-csi-node-info.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/rbac-csi-attacher.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/rbac-csi-driver-registrar.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/rbac-csi-provisioner.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/rbac-csi-snapshotter.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/csi-azurefile-provisioner.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/csi-azurefile-attacher.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/deploy/azurefile-csi-driver.yaml
+    echo "AzureFile csi driver installed";    
+fi
+
+# Fetching ginkgo for running the test
+GO111MODULE=off go get -u github.com/onsi/ginkgo/ginkgo
+# Exporting KUBECONFIG path
+export KUBECONFIG=$HOME/.kube/config
+# Running the e2e test
+ginkgo test/e2e
+# Checking for test status
+TEST_PASS=$?
+if [[ $TEST_PASS -ne 0 ]]; then
+    exit 1
+fi
