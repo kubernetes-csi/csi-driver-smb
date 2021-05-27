efad3dc24a07eb77285958c61329439bf58595eb
Julie Qi <lizhuqi@google.com>
2021-05-26 17:14:04 -0700

    Add a script to run e2e test

---
+++ test/run-e2e-test.sh
@@ -0,0 +1,30 @@
+# Copyright 2020 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+set -euo pipefail
+
+setup_e2e() {
+    # If run in prow, need to use kubernetes_e2e.py to set up the project and kubernetes automatically.
+    # If run locally, start a k8s cluster with Windows nodes.
+    make e2e-bootstrap
+    make install-smb-provisioner
+    make create-metrics-svc
+}
+
+export TEST_WINDOWS=true
+
+setup_e2e()
+make e2e-test
