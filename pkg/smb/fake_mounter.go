2fbbec19c65a49bdaa218ffd3364138ca12cfacd
andyzhangx <xiazhang@microsoft.com>
2023-12-05 14:26:40 +0000

    test: fix ut

--- pkg/smb/fake_mounter.go
+++ pkg/smb/fake_mounter.go
@@ -65,7 +65,7 @@ func (f *fakeMounter) IsLikelyNotMountPoint(file string) (bool, error) {
 
 func NewFakeMounter() (*mount.SafeFormatAndMount, error) {
 	if runtime.GOOS == "windows" {
-		return mounter.NewSafeMounter(true, true)
+		return mounter.NewSafeMounter(false, true)
 	}
 	return &mount.SafeFormatAndMount{
 		Interface: &fakeMounter{},
